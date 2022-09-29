ca751f9f18131acdf7ddc66c57c6d5ee80af5175
Hobie Li <56865011+hobielee42@users.noreply.github.com>
2022-09-28 18:08:39 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f22t01/Shape.java
+++ app/src/main/java/com/example/cmput301f22t01/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301f22t01;
 public abstract class Shape {
     private int x;
     private int y;
-    private String color = "blue";
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
